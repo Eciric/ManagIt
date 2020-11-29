7b558becfaee464063b39c0ed549421bde23e606
Przemyslaw Kaczmarski <222452@edu.p.lodz.pl>
2020-11-29 17:53:01 +0100

    Selector Update

--- app/src/main/java/res/managit/whSelectorFragment.java
+++ app/src/main/java/res/managit/whSelectorFragment.java
@@ -2,57 +2,54 @@ package res.managit;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link whSelectorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class whSelectorFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ListView listView;
+    ArrayList<String> dbNames;
 
     public whSelectorFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment whSelectorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static whSelectorFragment newInstance(String param1, String param2) {
-        whSelectorFragment fragment = new whSelectorFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //NOTE(Przemek): Add database integration here, populate the dbNames with actual existing databases names.
+        listView = view.findViewById(R.id.listview);
+        dbNames = new ArrayList<>();
+        dbNames.add("Warehouse1");
+        dbNames.add("Warehouse2");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), R.layout.listview_text_formatter, R.id.textView2, dbNames);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //TODO(Przemek): I will expand this soon to another fragment with the selected database
+                Toast.makeText(getContext(), "Selected: " + dbNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
