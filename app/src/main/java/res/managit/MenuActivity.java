2a6b1db299e5a0745b73af4c783c04bdd3dc1d4d
Piotr Kalota <222456@edu.p.lodz.pl>
2020-12-10 00:32:54 +0100

    [Controller] Add navigation to menu fragments

--- app/src/main/java/res/managit/MenuActivity.java
+++ app/src/main/java/res/managit/MenuActivity.java
@@ -14,7 +14,7 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -29,6 +29,57 @@ public class MenuActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new statisticsFragment()).commit();
+        
+        drawer = findViewById(R.id.drawer_activity);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.nav_planer:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new planerFragment()).commit();
+                break;
+            case R.id.nav_add:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new addFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new settingsFragment()).commit();
+                break;
+            case R.id.nav_statistics:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new statisticsFragment()).commit();
+                break;
+            case R.id.nav_export:
+                Toast.makeText(this, "Export selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_quit:
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("EXIT", true);
+                startActivity(intent);
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
