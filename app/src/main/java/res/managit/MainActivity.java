8a202c94b599a2b9935e61cd291d65bfad79edc3
Krystian <222474@edu.p.lodz.pl>
2020-12-12 02:28:47 +0100

    Added saving and loading database names

--- app/src/main/java/res/managit/MainActivity.java
+++ app/src/main/java/res/managit/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
 
 import res.managit.dbo.PublicDatabaseAcces;
 import res.managit.dbo.WarehouseDb;
@@ -12,6 +13,9 @@ import res.managit.dbo.entity.Category;
 import res.managit.dbo.entity.Product;
 
 import static res.managit.dbo.DatabaseFunctions.createDatabase;
+import static res.managit.dbo.DatabaseFunctions.reloadDatabaseNames;
+import static res.managit.dbo.DatabaseFunctions.reloadDatabases;
+import static res.managit.dbo.DatabaseFunctions.saveDatabaseNames;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,8 +27,11 @@ public class MainActivity extends AppCompatActivity {
         PublicDatabaseAcces publicDatabaseAcces = new PublicDatabaseAcces();
         PublicDatabaseAcces.databaseList = new ArrayList<WarehouseDb>();
         PublicDatabaseAcces.databaseNameList = new ArrayList<String>();
-        
+        reloadDatabaseNames(getApplicationContext(),"DatabaseNames.csv");
+        reloadDatabases(getApplicationContext());
+
         if (getIntent().getBooleanExtra("EXIT", false)) {
+            saveDatabaseNames(getApplicationContext(),"DatabaseNames.csv");
             finish();
         }
     }
--- app/src/main/java/res/managit/dbo/DatabaseFunctions.java
+++ app/src/main/java/res/managit/dbo/DatabaseFunctions.java
@@ -4,6 +4,26 @@ import android.content.Context;
 
 import androidx.room.Room;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.xml.transform.Source;
+
 public abstract class DatabaseFunctions {
     public static WarehouseDb createDatabase(Context context, String name){
         if (PublicDatabaseAcces.databaseNameList.contains(name))
@@ -13,5 +33,45 @@ public abstract class DatabaseFunctions {
         PublicDatabaseAcces.databaseList.add(Db);
         PublicDatabaseAcces.databaseNameList.add(name);
         return Db;
+    }
+    public static void reloadDatabases(Context context){
+        for(String name : PublicDatabaseAcces.databaseNameList){
+            WarehouseDb Db = Room.databaseBuilder(context, WarehouseDb.class, name).build();
+            PublicDatabaseAcces.databaseList.add(Db);
+        }
+    }
+    public static void reloadDatabaseNames(Context context, String path){
+
+        try {
+            FileInputStream fis = context.openFileInput(path);
+            StringBuffer fileContent = new StringBuffer("");
+            byte[] bytes = new byte[1024];
+            int n;
+            while ((n = fis.read(bytes)) != -1){
+                fileContent.append(new String(bytes,0,n));
+            }
+            fis.close();
+            String[] tempNames = new String(fileContent).split(",");
+            for(String s : tempNames){
+                PublicDatabaseAcces.databaseNameList.add(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveDatabaseNames(Context context, String filePath){
+        try {
+            FileOutputStream fileOutputStream = context.openFileOutput(filePath, Context.MODE_PRIVATE);
+            StringBuilder data = new StringBuilder();
+            for(String s : PublicDatabaseAcces.databaseNameList){
+                data.append(s+",");
+            }
+            fileOutputStream.write(data.toString().getBytes());
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
