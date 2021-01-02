f69bd776ff45202eff32aed4d1f1b48aa58f558e
unknown <KS-coder-98>
2021-01-02 14:53:14 +0100

    reformat one row's view for events

--- app/src/main/java/res/managit/service/EventAdapter.java
+++ app/src/main/java/res/managit/service/EventAdapter.java
@@ -21,25 +21,22 @@ public class EventAdapter extends ArrayAdapter<Event> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        // Get the data item for this position
         Event event = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.one_row_event, parent, false);
         }
-        // Lookup view for data population
         TextView action = (TextView) convertView.findViewById(R.id.action);
-        TextView amount = (TextView) convertView.findViewById(R.id.amount);
         TextView date = (TextView) convertView.findViewById(R.id.date);
 
-        //        TextView action = (TextView) convertView.findViewById(R.id.tvHome);
-        // Populate the data into the template view using the data object
-        action.setText(event.action);
-        String temp = "amount " + event.amount;
-        amount.setText(temp);
-        date.setText(event.date.toString());
 
-        // Return the completed view to render on screen
+        String textAction = "Type: " + event.action;
+        String textDate = "Date " + event.date.getYear() + " " + event.getDate().getMonth() + " " + event.getDate().getDayOfWeek() + " " + event.getDate().getHour() + ":" + event.getDate().getMinute();
+        if (event.getDate().getMinute() == 0)
+            textDate += "0";
+
+        action.setText(textAction);
+        date.setText(textDate);
+
         return convertView;
     }
 
