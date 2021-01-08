753aa5a34e8b4ca52fd5f9e84573037edfc3aaf9
Piotr Kalota <222456@edu.p.lodz.pl>
2021-01-08 20:20:55 +0100

    Add categories fragment

---
+++ app/src/main/java/res/managit/categoriesFragment.java
@@ -0,0 +1,41 @@
+package res.managit;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import res.managit.dbo.PublicDatabaseAcces;
+import res.managit.service.CategoryListRetriever;
+
+public class categoriesFragment extends Fragment {
+    public categoriesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        new CategoryListRetriever(requireContext(), view, PublicDatabaseAcces.currentDatabase).execute();
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
+        toolbar.setTitle("Categories");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
