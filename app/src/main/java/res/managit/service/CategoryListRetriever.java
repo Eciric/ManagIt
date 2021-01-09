af7ada0c4be1fddfa89a9298432df94e01b281e2
Piotr Kalota <222456@edu.p.lodz.pl>
2021-01-09 18:02:29 +0100

    Category list view imporvement

--- app/src/main/java/res/managit/service/CategoryListRetriever.java
+++ app/src/main/java/res/managit/service/CategoryListRetriever.java
@@ -2,9 +2,12 @@ package res.managit.service;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +20,13 @@ public class CategoryListRetriever extends AsyncTask<Void, Void, List<Category>>
     WarehouseDb db;
     View view;
     Context context;
+    LayoutInflater inflater;
 
-    public CategoryListRetriever(Context context, View view, WarehouseDb db) {
+    public CategoryListRetriever(Context context, View view, WarehouseDb db, LayoutInflater inflater) {
         this.db = db;
         this.view = view;
         this.context = context;
+        this.inflater = inflater;
     }
 
     @Override
@@ -32,16 +37,54 @@ public class CategoryListRetriever extends AsyncTask<Void, Void, List<Category>>
     @Override
     protected void onPostExecute(List<Category> result) {
         ListView products = view.findViewById(R.id.list);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(context, R.layout.listview_text_formatter, R.id.textView2, createCategoryLabels(result));
+        List<Data> data = createCategoryLabels(result);
+        ArrayAdapter<Data> arrayAdapter = new ArrayAdapter<Data>(context, R.layout.manage_listview_formatter, R.id.elementId, data) {
+            @Override
+            public View getView(int position,
+                                View convertView,
+                                ViewGroup parent) {
+                if(convertView == null)
+                    convertView = inflater.inflate(R.layout.manage_listview_formatter, null, false);
+
+                Data d = data.get(position);
+                TextView id = convertView.findViewById(R.id.elementId);
+                TextView name = convertView.findViewById(R.id.elementName);
+                TextView info = convertView.findViewById(R.id.elementInfo);
+
+                id.setText(Long.toString(d.getId()));
+                name.setText(d.getName());
+                info.setVisibility(View.INVISIBLE);
+
+                return convertView;
+            }
+        };
         products.setAdapter(arrayAdapter);
     }
 
-    private List<String> createCategoryLabels(List<Category> result) {
-        List<String> categories = new ArrayList<>();
-        for (Category c : result) {
-            categories.add("[" + c.getCategoryId() + "] " + c.getName());
+    class Data {
+        long id;
+        String name;
+
+        public Data(long id, String name) {
+            this.id = id;
+            this.name = name;
         }
-        return categories;
+
+        public long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private List<Data> createCategoryLabels(List<Category> result) {
+        List<Data> data = new ArrayList<>();
+        for (Category c : result) {
+            data.add(new Data(c.getCategoryId(), c.getName()));
+        }
+        return data;
     }
 
 }
