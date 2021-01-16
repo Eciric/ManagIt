8f1182569a090baee62d5110e399ca4488167b75
Piotr Kalota <222456@edu.p.lodz.pl>
2021-01-16 13:35:03 +0100

    Add javadoc to ProductSpinnerInitializer

--- app/src/main/java/res/managit/service/ProductSpinnerInitializer.java
+++ app/src/main/java/res/managit/service/ProductSpinnerInitializer.java
@@ -14,35 +14,52 @@ import res.managit.R;
 import res.managit.dbo.WarehouseDb;
 import res.managit.dbo.entity.Category;
 
+/**
+ * Class used to initialize spinner from product's add card
+ */
 public class ProductSpinnerInitializer extends AsyncTask<Void, Void, List<Category>> {
     WarehouseDb db;
     View view;
     Context context;
 
+
+    /**
+     * Class constructor
+     * @param context fragment's context
+     * @param view fragment's view
+     * @param db database on which operations will be done
+     */
     public ProductSpinnerInitializer(Context context, View view, WarehouseDb db) {
         this.db = db;
         this.view = view;
         this.context = context;
     }
 
+    /**
+     * Function used to retrieve categories from database
+     * @return categories list
+     */
     @Override
     protected List<Category> doInBackground(Void... voids) {
         return db.categoryDao().getAll();
     }
 
+    /**
+     * Function used to populate spinner with data from result list
+     */
     @Override
     protected void onPostExecute(List<Category> result) {
         Spinner spinner = view.findViewById(R.id.productCategorySpinner);
-
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, createCategoryNameList(result));
-
-        // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
         spinner.setAdapter(dataAdapter);
     }
 
+    /**
+     * Function which converts list of categories to list of categories names
+     * @param result list of categories
+     * @return list of categories names
+     */
     private List<String> createCategoryNameList(List<Category> result) {
         List<String> categories = new ArrayList<>();
         for (Category c : result) {
