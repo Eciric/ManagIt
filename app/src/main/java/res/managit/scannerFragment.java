2863d88aff913d79a901ba56af765e250fa1eabb
KS-coder-98 <58855356+KS-coder-98@users.noreply.github.com>
2021-01-23 23:31:23 +0100

    espanol working version xdddd

--- app/src/main/java/res/managit/scannerFragment.java
+++ app/src/main/java/res/managit/scannerFragment.java
@@ -1,14 +1,27 @@
 package res.managit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class scannerFragment extends Fragment {
+
+
+    EditText etCodigo;
+    Button btnLeerCodigo;
+
     public scannerFragment() {
         // Required empty public constructor
     }
@@ -22,6 +35,47 @@ public class scannerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scaner, container, false);
+        View view = inflater.inflate(R.layout.fragment_scaner, container, false);
+
+        etCodigo = view.findViewWithTag(R.id.etCodigo);
+        btnLeerCodigo = view.findViewById(R.id.btnLeerCodigo);
+
+        btnLeerCodigo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                escaner();
+            }
+        });
+
+        return view;
+    }
+
+    public void escaner(){
+        IntentIntegrator intent = IntentIntegrator.forSupportFragment(scannerFragment.this);
+        //IntentIntegrator intent = new IntentIntegrator(getActivity());
+        intent.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        intent.setPrompt("ESCANEAR CODIGO");
+        intent.setCameraId(0);
+        intent.setBeepEnabled(false);
+        intent.setBarcodeImageEnabled(false);
+        intent.initiateScan();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(getContext(), "Cancelaste el escaneo", Toast.LENGTH_SHORT).show();
+            } else {
+//                etCodigo.setText(result.getContents().toString());
+                if ( result != null ){
+                    System.err.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+result.getContents());
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
